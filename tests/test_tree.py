3636af6f1813d5c8d3ab014dc7a5dba49258d511
Brady Johnston <brady.johnston@me.com>
2026-01-14 16:55:40 +0000

    Initializing gh-pages branch

---
+++ tests/test_tree.py
@@ -0,0 +1,33 @@
+import bpy
+from nodebpy import TreeBuilder
+from nodebpy import nodes as n
+from nodebpy import sockets as s
+
+
+def test_create_tree_and_save():
+    with TreeBuilder("AnotherTree") as tree:
+        tree.interface(
+            inputs=[s.SocketInt("Count")],
+            outputs=[s.SocketGeometry("Instances")],
+        )
+
+        rotation = (
+            n.RandomValue.vector(min=(-1, -1, -1), seed=2)
+            >> n.AlignRotationToVector()
+            >> n.RotateRotation(
+                rotate_by=n.AxisAngleToRotation(angle=0.3), rotation_space="LOCAL"
+            )
+        )
+
+        _ = (
+            tree.inputs.count
+            >> n.Points(position=n.RandomValue.vector(min=(-1, -1, -1)))
+            >> n.InstanceOnPoints(instance=n.Cube(), rotation=rotation)
+            >> n.SetPosition(
+                position=n.Position() * 2.0 + (0, 0.2, 0.3),
+                offset=(0, 0, 0.1),
+            )
+            >> n.RealizeInstances()
+            >> n.InstanceOnPoints(n.Cube(), instance=...)
+            >> tree.outputs.instances
+        )
