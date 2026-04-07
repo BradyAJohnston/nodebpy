ea0b970e2dff765c4790a9d75c777a2a652f642c
Brady Johnston <brady.johnston@me.com>
2026-04-07 17:20:16 +0100

    cleanup screenshot code

--- src/nodebpy/__init__.py
+++ src/nodebpy/__init__.py
@@ -1,13 +1,7 @@
 from . import nodes, screenshot, sockets
 from .builder import TreeBuilder
 from .nodes import compositor, geometry, shader
-from .screenshot import (
-    apply_tree,
-    display_render,
-    generate_mermaid_diagram,
-    render_preview,
-    save_mermaid_diagram,
-)
+from .screenshot import generate_mermaid_diagram, save_mermaid_diagram
 
 __all__ = [
     "nodes",
@@ -17,9 +11,6 @@ __all__ = [
     "sockets",
     "screenshot",
     "TreeBuilder",
-    "apply_tree",
-    "display_render",
     "generate_mermaid_diagram",
-    "render_preview",
     "save_mermaid_diagram",
 ]
