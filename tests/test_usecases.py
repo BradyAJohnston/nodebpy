7b536b49082ab250f97de653990032a7655317bc
Brady Johnston <brady.johnston@me.com>
2026-04-29 16:49:08 +0100

    some MiN-related tests

--- tests/test_usecases.py
+++ tests/test_usecases.py
@@ -1,10 +1,13 @@
+import itertools
 import math
 from functools import reduce
-from itertools import product
-from operator import and_
+from itertools import combinations, product
+from operator import and_, or_
 
 import bpy
+from bpy.types import GeometryNodeTree
 
+from nodebpy import TreeBuilder
 from nodebpy import geometry as g
 from nodebpy.builder import FloatSocket
 from nodebpy.nodes.geometry.groups import PrincipalComponents
@@ -126,3 +129,166 @@ def test_eulers_number():
         )
 
     assert isinstance(zone.input.i.value, FloatSocket)
+
+
+class CompareGenerationMethods:
+    def test_compare_methods(self, snapshot):
+        trees = [
+            getattr(self, method)()
+            for method in [x for x in dir(self) if x.startswith("method_")]
+        ]
+        for tree in trees:
+            with tree as tree:
+                tree.arrange()
+
+        assert len(trees[0]) == len(trees[1])
+        assert len(trees[0].tree.links) == len(trees[1].tree.links)
+
+
+class TestCompareMiNCleanGridVerts(CompareGenerationMethods):
+    def method_nodebpy_gridverts(self) -> TreeBuilder:
+        with g.tree() as tree:
+            extent = tree.inputs.vector(
+                "Extent (unit)",
+                (7.0, 5.0, 4.0),
+                min_value=0.0,
+                max_value=1_000_000,
+            )
+            world = tree.inputs.vector(
+                "World per Unit",
+                (1e-6, 1e-6, 1e-6),
+                min_value=0.0,
+            )
+
+            pos = g.Position().o.position
+            mul = extent * world
+
+            equals = []
+            for x in [-0.5, 0.5]:
+                comp = g.VectorMath.multiply(mul, x).o.vector
+                equals.append(
+                    (g.Compare.float.equal(a, b, 0.001) for a, b in zip(comp, pos))
+                )
+
+            ors = [a | b for a, b in zip(*equals)]
+            ands = [a & b for a, b in combinations(ors, 2)]
+            final = reduce(or_, ands)
+            final >> tree.outputs.boolean()
+        return tree
+
+    def method_api_gridverts(self) -> TreeBuilder:
+        node_group = bpy.data.node_groups.get("_grid_verts")
+        if node_group:
+            return node_group
+
+        node_group = bpy.data.node_groups.new(
+            type="GeometryNodeTree", name="_grid_verts"
+        )
+        links = node_group.links
+        interface = node_group.interface
+
+        interface.new_socket(
+            "Extent (unit)", in_out="INPUT", socket_type="NodeSocketVector"
+        )
+        interface.items_tree[-1].default_value = (7.0, 5.0, 4.0)
+        interface.items_tree[-1].min_value = 0.0
+        interface.items_tree[-1].max_value = 10000000.0
+        interface.items_tree[-1].attribute_domain = "POINT"
+
+        interface.new_socket(
+            "World per Unit", in_out="INPUT", socket_type="NodeSocketVector"
+        )
+        interface.items_tree[-1].default_value = (1e-6, 1e-6, 1e-6)
+        interface.items_tree[-1].min_value = 0.0
+        interface.items_tree[-1].max_value = 3.4028234663852886e38
+        interface.items_tree[-1].attribute_domain = "POINT"
+
+        interface.new_socket("Boolean", in_out="OUTPUT", socket_type="NodeSocketBool")
+        interface.items_tree[-1].attribute_domain = "POINT"
+
+        group_input = node_group.nodes.new("NodeGroupInput")
+        group_input.location = (-1000, 0)
+
+        group_output = node_group.nodes.new("NodeGroupOutput")
+        group_output.location = (850, 100)
+
+        extent_world = node_group.nodes.new("ShaderNodeVectorMath")
+        extent_world.operation = "MULTIPLY"
+        extent_world.location = (-620, -220)
+        links.new(group_input.outputs["Extent (unit)"], extent_world.inputs[0])
+        links.new(group_input.outputs["World per Unit"], extent_world.inputs[1])
+
+        pos = node_group.nodes.new("GeometryNodeInputPosition")
+        pos.location = (-620, 140)
+
+        pos_xyz = node_group.nodes.new("ShaderNodeSeparateXYZ")
+        pos_xyz.location = (-420, 140)
+        links.new(pos.outputs[0], pos_xyz.inputs[0])
+
+        boundary_compares = [[], [], []]
+
+        for ix, side in enumerate(["min", "max"]):
+            loc = node_group.nodes.new("ShaderNodeVectorMath")
+            loc.operation = "MULTIPLY"
+            loc.location = (-620, -80 - 170 * ix)
+            links.new(extent_world.outputs[0], loc.inputs[0])
+            loc.inputs[1].default_value = (
+                (-0.5, -0.5, -0.5) if side == "min" else (0.5, 0.5, 0.5)
+            )
+
+            loc_xyz = node_group.nodes.new("ShaderNodeSeparateXYZ")
+            loc_xyz.location = (-420, -80 - 170 * ix)
+            links.new(loc.outputs[0], loc_xyz.inputs[0])
+
+            for axix in range(3):
+                compare = node_group.nodes.new("FunctionNodeCompare")
+                compare.data_type = "FLOAT"
+                compare.operation = "EQUAL"
+                compare.mode = "ELEMENT"
+                compare.location = (-210, 320 - (ix * 3 + axix) * 140)
+                links.new(pos_xyz.outputs[axix], compare.inputs[1])
+                links.new(loc_xyz.outputs[axix], compare.inputs[0])
+                boundary_compares[axix].append(compare)
+
+        on_boundary = []
+        for axix in range(3):
+            ornode = node_group.nodes.new("FunctionNodeBooleanMath")
+            ornode.operation = "OR"
+            ornode.location = (10, 100 - axix * 140)
+            links.new(boundary_compares[axix][0].outputs[0], ornode.inputs[0])
+            links.new(boundary_compares[axix][1].outputs[0], ornode.inputs[1])
+            on_boundary.append(ornode)
+
+        edge_xy = node_group.nodes.new("FunctionNodeBooleanMath")
+        edge_xy.operation = "AND"
+        edge_xy.location = (220, 120)
+        links.new(on_boundary[0].outputs[0], edge_xy.inputs[0])
+        links.new(on_boundary[1].outputs[0], edge_xy.inputs[1])
+
+        edge_yz = node_group.nodes.new("FunctionNodeBooleanMath")
+        edge_yz.operation = "AND"
+        edge_yz.location = (220, -20)
+        links.new(on_boundary[1].outputs[0], edge_yz.inputs[0])
+        links.new(on_boundary[2].outputs[0], edge_yz.inputs[1])
+
+        edge_zx = node_group.nodes.new("FunctionNodeBooleanMath")
+        edge_zx.operation = "AND"
+        edge_zx.location = (220, -160)
+        links.new(on_boundary[2].outputs[0], edge_zx.inputs[1])
+        links.new(on_boundary[0].outputs[0], edge_zx.inputs[0])
+
+        edge_or_1 = node_group.nodes.new("FunctionNodeBooleanMath")
+        edge_or_1.operation = "OR"
+        edge_or_1.location = (420, 80)
+        links.new(edge_xy.outputs[0], edge_or_1.inputs[0])
+        links.new(edge_yz.outputs[0], edge_or_1.inputs[1])
+
+        edge_or_2 = node_group.nodes.new("FunctionNodeBooleanMath")
+        edge_or_2.operation = "OR"
+        edge_or_2.location = (620, 80)
+        links.new(edge_or_1.outputs[0], edge_or_2.inputs[0])
+        links.new(edge_zx.outputs[0], edge_or_2.inputs[1])
+
+        links.new(edge_or_2.outputs[0], group_output.inputs["Boolean"])
+
+        return TreeBuilder.geometry(node_group)
