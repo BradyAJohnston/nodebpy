44d1b3b6bf678be6ac190b0db0475f20bf166564
Brady Johnston <brady.johnston@me.com>
2026-08-24 08:39:28 +0100

    make generate

--- src/nodebpy/nodes/compositor/vector.py
+++ src/nodebpy/nodes/compositor/vector.py
@@ -5,7 +5,9 @@ from typing import TYPE_CHECKING
 import bpy
 
 from ...builder import BaseNode, SocketAccessor
-from ...builder.socket import FloatSocket
+from ...builder.socket import (
+    FloatSocket,
+)
 from ...types import (
     InputFloat,
 )
--- src/nodebpy/nodes/geometry/color.py
+++ src/nodebpy/nodes/geometry/color.py
@@ -5,7 +5,10 @@ from typing import TYPE_CHECKING
 import bpy
 
 from ...builder import BaseNode, SocketAccessor
-from ...builder.socket import ColorSocket, FloatSocket
+from ...builder.socket import (
+    ColorSocket,
+    FloatSocket,
+)
 from ...types import (
     InputColor,
     InputFloat,
