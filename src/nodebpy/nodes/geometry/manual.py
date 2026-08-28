aa3eadfb48304d19cfc6453dc9bc01d81e2eb5da
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2026-08-28 15:47:33 +0100

    Properly support Font and Sound sockets (#139)
    * Properly support Font and Sound sockets
    
    * render

--- src/nodebpy/nodes/geometry/manual.py
+++ src/nodebpy/nodes/geometry/manual.py
@@ -1427,6 +1427,18 @@ class IndexSwitch[T: BaseSocket](ItemsMixin, BaseNode):
     ) -> "IndexSwitch[ClosureSocket]":
         return IndexSwitch(index=index, items=items, data_type="CLOSURE")
 
+    @classmethod
+    def font(
+        cls, index: InputInteger = 0, items: Iterable[InputFont] = ()
+    ) -> "IndexSwitch[FontSocket]":
+        return IndexSwitch(index=index, items=items, data_type="FONT")
+
+    @classmethod
+    def sound(
+        cls, index: InputInteger = 0, items: Iterable[InputSound] = ()
+    ) -> "IndexSwitch[SoundSocket]":
+        return IndexSwitch(index=index, items=items, data_type="SOUND")
+
     class _Inputs(SocketAccessor):
         index: IntegerSocket
 
@@ -1804,6 +1816,22 @@ class MenuSwitch[T: BaseSocket](_MenuSwitchBase[T]):
         items: MenuItems[InputClosure] | None = None,
     ) -> "MenuSwitch[ClosureSocket]":
         return MenuSwitch(menu, items, data_type="CLOSURE")
+
+    @classmethod
+    def font(
+        cls,
+        menu: InputMenu = None,
+        items: dict[str, InputFont] | None = None,
+    ) -> "MenuSwitch[FontSocket]":
+        return MenuSwitch(menu, items, data_type="FONT")
+
+    @classmethod
+    def sound(
+        cls,
+        menu: InputMenu = None,
+        items: dict[str, InputSound] | None = None,
+    ) -> "MenuSwitch[SoundSocket]":
+        return MenuSwitch(menu, items, data_type="SOUND")
 
 
 class CaptureAttribute(ItemsMixin, BaseNode):
